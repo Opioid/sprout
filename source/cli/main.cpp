b317d0f360d18268f5f6d72d147507f5222676df
beni <benjamin.savs@gmail.com>
2015-03-17 09:46:16 +0100

    Added timing to cli

--- source/cli/main.cpp
+++ source/cli/main.cpp
@@ -7,9 +7,18 @@
 #include "core/image/storage/writer.hpp"
 #include "base/math/vector.inl"
 #include <iostream>
+#include <chrono>
+
+float duration_to_seconds(std::chrono::high_resolution_clock::duration duration);
 
 int main() {
 	std::cout << "Welcome to sprout!" << std::endl;
+
+	std::chrono::high_resolution_clock clock;
+
+	std::cout << "Loading..." << std::endl;
+
+	auto loading_start = clock.now();
 
 	std::string takename = "../data/takes/imrod.take";
 	take::Loader take_loader;
@@ -27,17 +36,37 @@ int main() {
 		return 1;
 	}
 
+	auto loading_duration = clock.now() - loading_start;
+	std::cout << "(" << duration_to_seconds(loading_duration) << "s)" << std::endl;
+
 	rendering::Renderer renderer(take->surface_integrator_factory);
+
+	std::cout << "Rendering..." << std::endl;
+
+	auto rendering_start = clock.now();
 
 	renderer.render(scene, take->context);
 
+	auto rendering_duration = clock.now() - rendering_start;
+	std::cout << "(" << duration_to_seconds(rendering_duration) << "s)" << std::endl;
+
+	std::cout << "Exporting..." << std::endl;
+
+	auto exporting_start = clock.now();
+
 	bool result = image::write("output.png", take->context.camera->film().resolve());
 
-	if (result) {
-		std::cout << "We wrote output.png" << std::endl;
-	} else {
+	if (!result) {
 		std::cout << "Something went wrong" << std::endl;
 	}
 
+	auto exporting_duration = clock.now() - exporting_start;
+	std::cout << "(" << duration_to_seconds(exporting_duration) << "s)" << std::endl;
+
 	return 0;
+}
+
+float duration_to_seconds(std::chrono::high_resolution_clock::duration duration) {
+	std::chrono::milliseconds milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+	return static_cast<float>(milliseconds.count()) / 1000.f;
 }
