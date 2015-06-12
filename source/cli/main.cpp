9f378aee23c11fcb1111c56b3453f3631169d0ce
Benjamin Savs <benjamin.savs@gmail.com>
2015-06-12 20:07:22 +0200

    Merge

--- source/cli/main.cpp
+++ source/cli/main.cpp
@@ -31,7 +31,7 @@ int main() {
 
 	auto loading_start = clock.now();
 
-	std::string takename = "../data/takes/sponza_0.take";
+	std::string takename = "../data/takes/mask.take";
 
 	std::shared_ptr<take::Take> take;
 
