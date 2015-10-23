68e75c487a08778b57a5893ac26ce83eeede689c
beni <benjamin.savs@gmail.com>
2015-10-23 17:26:39 +0200

    Animation experiment

--- source/core/scene/entity/keyframe.cpp
+++ source/core/scene/entity/keyframe.cpp
@@ -7,15 +7,24 @@ namespace scene { namespace entity {
 void Keyframe::interpolate(const Keyframe& other, float t, Keyframe& result) const {
 	result.transformation = math::lerp(transformation, other.transformation, t);
 
-	if (morphing.targets[0] != other.morphing.targets[0]
-	||  morphing.targets[1] != other.morphing.targets[1]) {
+	if (morphing.targets[0]       == morphing.targets[1]
+	&&  other.morphing.targets[0] == other.morphing.targets[1]) {
+		result.morphing.weight = t;
+
+		result.morphing.targets[0] = morphing.targets[0];
+		result.morphing.targets[1] = other.morphing.targets[0];
+	}
+/*	else if (morphing.targets[0] != other.morphing.targets[0]
+		 ||  morphing.targets[1] != other.morphing.targets[1]) {
 		result.morphing.weight = other.morphing.weight;
-	} else {
+	}*/ else {
 		result.morphing.weight = math::lerp(morphing.weight, other.morphing.weight, t);
+
+		result.morphing.targets[0] = other.morphing.targets[0];
+		result.morphing.targets[1] = other.morphing.targets[1];
 	}
 
-	result.morphing.targets[0] = other.morphing.targets[0];
-	result.morphing.targets[1] = other.morphing.targets[1];
+
 }
 
 }}
