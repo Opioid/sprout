c931f99b163ee7454f8439a2425f224bd7a765f1
Opioid <benjamin.savs@gmail.com>
2016-07-23 22:34:28 +0200

    Maintenance

--- source/core/scene/material/substitute/substitute_base_sample.cpp
+++ source/core/scene/material/substitute/substitute_base_sample.cpp
@@ -55,14 +55,13 @@ float3 Sample_base::Layer::base_evaluate(float3_p wi, float3_p wo, float& pdf) c
 	float n_dot_wo = clamped_n_dot(wo);
 
 	float d_pdf;
-	float3 d_reflection = disney::Isotropic::evaluate(wi, wo, n_dot_wi, n_dot_wo,
-													  *this, d_pdf);
+	float3 d_reflection = disney::Isotropic::evaluate(wi, wo, n_dot_wi, n_dot_wo, *this, d_pdf);
 
 	fresnel::Schlick schlick(f0);
 	float3 ggx_fresnel;
 	float  ggx_pdf;
-	float3 ggx_reflection = ggx::Isotropic::evaluate(wi, wo, n_dot_wi, n_dot_wo,
-													 *this, schlick, ggx_fresnel, ggx_pdf);
+	float3 ggx_reflection = ggx::Isotropic::evaluate(wi, wo, n_dot_wi, n_dot_wo, *this,
+													 schlick, ggx_fresnel, ggx_pdf);
 
 	pdf = 0.5f * (d_pdf + ggx_pdf);
 
@@ -70,10 +69,9 @@ float3 Sample_base::Layer::base_evaluate(float3_p wi, float3_p wo, float& pdf) c
 }
 
 void Sample_base::Layer::diffuse_sample(float3_p wo, sampler::Sampler& sampler,
-												   bxdf::Result& result) const {
+										bxdf::Result& result) const {
 	float n_dot_wo = clamped_n_dot(wo);
-	float n_dot_wi = disney::Isotropic::sample(wo, n_dot_wo, *this,
-														  sampler, result);
+	float n_dot_wi = disney::Isotropic::sample(wo, n_dot_wo, *this, sampler, result);
 
 	fresnel::Schlick schlick(f0);
 	float3 ggx_fresnel;
@@ -86,29 +84,28 @@ void Sample_base::Layer::diffuse_sample(float3_p wo, sampler::Sampler& sampler,
 }
 
 void Sample_base::Layer::specular_sample(float3_p wo, sampler::Sampler& sampler,
-													bxdf::Result& result) const {
+										 bxdf::Result& result) const {
 	float n_dot_wo = clamped_n_dot(wo);
 
 	fresnel::Schlick schlick(f0);
 	float3 ggx_fresnel;
 	float n_dot_wi = ggx::Isotropic::sample(wo, n_dot_wo, *this, schlick,
-													   sampler, ggx_fresnel, result);
+											sampler, ggx_fresnel, result);
 
 	float d_pdf;
-	float3 d_reflection = disney::Isotropic::evaluate(result.wi, wo, n_dot_wi, n_dot_wo,
-													  *this, d_pdf);
+	float3 d_reflection = disney::Isotropic::evaluate(result.wi, wo, n_dot_wi,
+													  n_dot_wo, *this, d_pdf);
 
 	result.reflection = n_dot_wi * (result.reflection + (1.f - ggx_fresnel) * d_reflection);
 	result.pdf = 0.5f * (result.pdf + d_pdf);
 }
 
 void Sample_base::Layer::pure_specular_sample(float3_p wo, sampler::Sampler& sampler,
-														 bxdf::Result& result) const {
+											  bxdf::Result& result) const {
 	float n_dot_wo = clamped_n_dot(wo);
 
 	fresnel::Schlick schlick(f0);
-	float n_dot_wi = ggx::Isotropic::sample(wo, n_dot_wo, *this,
-													   schlick, sampler, result);
+	float n_dot_wi = ggx::Isotropic::sample(wo, n_dot_wo, *this, schlick, sampler, result);
 	result.reflection *= n_dot_wi;
 }
 
