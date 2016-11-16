a085937267ef845df6a428b93d3b74d3836605f0
Opioid <benjamin.savs@gmail.com>
2016-11-16 22:28:41 +0100

    Disabled a codeblock in morphable mesh loader

--- source/core/scene/shape/triangle/triangle_mesh_provider.cpp
+++ source/core/scene/shape/triangle/triangle_mesh_provider.cpp
@@ -174,22 +174,22 @@ std::shared_ptr<Shape> Provider::load_morphable_mesh(const std::string& /*filena
 		}
 
 		// The idea is to have one identical set of indices for all morph targets
-		if (collection->triangles().empty()) {
-			auto& triangles = handler.triangles();
+//		if (collection->triangles().empty()) {
+//			auto& triangles = handler.triangles();
 
-			for (auto& p : handler.parts()) {
-				uint32_t triangles_start = p.start_index / 3;
-				uint32_t triangles_end = (p.start_index + p.num_indices) / 3;
+//			for (auto& p : handler.parts()) {
+//				uint32_t triangles_start = p.start_index / 3;
+//				uint32_t triangles_end = (p.start_index + p.num_indices) / 3;
 
-				for (uint32_t i = triangles_start; i < triangles_end; ++i) {
-					uint32_t a = triangles[i].a;
-					uint32_t b = triangles[i].b;
-					uint32_t c = triangles[i].c;
+//				for (uint32_t i = triangles_start; i < triangles_end; ++i) {
+//					uint32_t a = triangles[i].a;
+//					uint32_t b = triangles[i].b;
+//					uint32_t c = triangles[i].c;
 
-					collection->triangles().push_back(Index_triangle{a, b, c, p.material_index});
-				}
-			}
-		}
+//					collection->triangles().push_back(Index_triangle{a, b, c, p.material_index});
+//				}
+//			}
+//		}
 
 		collection->add_swap_vertices(handler.vertices());
 	}
