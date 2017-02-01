555606502c2557674d21dcbc2285362eac756326
Opioid <benjamin.savs@gmail.com>
2017-02-01 10:35:42 +0100

    Tried one more thing

--- source/core/image/texture/sampler/bilinear.hpp
+++ source/core/image/texture/sampler/bilinear.hpp
@@ -42,10 +42,10 @@ inline float3 bilinear(float3_p c00, float3_p c01, float3_p c10, float3_p c11,
 }
 
 inline float3 bilinear(float3 c[4], float s, float t) {
-	float _s = 1.f - s;
-	float _t = 1.f - t;
+//	float _s = 1.f - s;
+//	float _t = 1.f - t;
 
-	return _s * (_t * c[0] + t * c[1]) + s * (_t * c[2] + t * c[3]);
+//	return _s * (_t * c[0] + t * c[1]) + s * (_t * c[2] + t * c[3]);
 /*
 	using namespace math::simd;
 
@@ -77,6 +77,34 @@ inline float3 bilinear(float3 c[4], float s, float t) {
 
 	return result;
 	*/
+
+	using namespace math::simd;
+
+	Vector mmt = load_float(t);
+	Vector mms = load_float(s);
+	Vector mmot = load_float(1.f - t);
+	Vector mmos = load_float(1.f - s);
+
+	Vector mm00 = load_float3_unsafe(c[0]);
+	Vector mm01 = load_float3_unsafe(c[1]);
+	Vector mm10 = load_float3_unsafe(c[2]);
+	Vector mm11 = load_float3_unsafe(c[3]);
+
+	mm01 = mul3(mmt, mm01);
+	mm11 = mul3(mmt, mm11);
+	mm00 = mul3(mmot, mm00);
+	mm10 = mul3(mmot, mm10);
+
+	Vector mma = add3(mm00, mm01);
+	Vector mmb = add3(mm10, mm11);
+
+	mma = mul3(mmos, mma);
+	mmb = mul3(mms, mmb);
+
+	float3 result;
+	store_float3_unsafe(result, add3(mma, mmb));
+
+	return result;
 }
 
 inline float4 bilinear(float4_p c00, float4_p c01, float4_p c10, float4_p c11,
