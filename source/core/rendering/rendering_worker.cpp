dd16b77937ee9e697be59121e274831dddfe7659
Opioid <benjamin.savs@gmail.com>
2017-03-08 15:09:07 +0100

    Maintenance

--- source/core/rendering/rendering_worker.cpp
+++ source/core/rendering/rendering_worker.cpp
@@ -5,6 +5,7 @@
 #include "scene/scene.hpp"
 #include "scene/scene_ray.inl"
 #include "scene/prop.hpp"
+#include "scene/shape/node_stack.inl"
 #include "scene/scene_intersection.hpp"
 #include "scene/material/material.hpp"
 #include "base/math/vector4.inl"
