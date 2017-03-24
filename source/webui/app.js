13f3449c40b8cdd09e370daec5bcaf015736a718
Opioid <benjamin.savs@gmail.com>
2017-03-24 22:59:49 +0100

    Let-yfying webui

--- source/webui/app.js
+++ source/webui/app.js
@@ -1,6 +1,6 @@
 IterationCounter = function() {
 	this.count = 0;
-	this.label = document.getElementById('iterationLabel');
+	this.label = document.getElementById("iterationLabel");
 }
 
 IterationCounter.prototype.constructor = IterationCounter;
@@ -16,21 +16,21 @@ IterationCounter.prototype.update = function() {
 }
 
 IterationCounter.prototype.display = function() {
-	this.label.innerHTML = '# ' + this.count; 
+	this.label.innerHTML = "# " + this.count; 
 }
 
 FpsCounter = function() {
 	this.previousTime = 0.0;
 	this.frameTimeCounter = 0.0;
 	this.frameCounter = 0;
-	this.label = document.getElementById('fpsLabel');
+	this.label = document.getElementById("fpsLabel");
 }
 
 FpsCounter.prototype.constructor = FpsCounter;
 
 FpsCounter.prototype.update = function() {
-	var currentTime = performance.now();
-	var frameTime = (currentTime - this.previousTime) / 1000.0;
+	let currentTime = performance.now();
+	let frameTime = (currentTime - this.previousTime) / 1000.0;
 
 	this.previousTime = currentTime;
 
@@ -39,8 +39,8 @@ FpsCounter.prototype.update = function() {
 	this.frameCounter++;
 
 	if (this.frameTimeCounter > 1.0) {
-		var fps = Math.floor((this.frameCounter / this.frameTimeCounter) * 10.0) / 10.0;
-		this.label.innerHTML = fps + ' FPS';
+		let  fps = Math.floor((this.frameCounter / this.frameTimeCounter) * 10.0) / 10.0;
+		this.label.innerHTML = fps + " FPS";
 
 		this.frameTimeCounter = 0.0;
 		this.frameCounter = 0;
@@ -48,28 +48,28 @@ FpsCounter.prototype.update = function() {
 }
 
 window.onload = function() {
-	var consoleForm = document.getElementById('consoleForm');
-	var consoleInput = document.getElementById('consoleInput');
+	let consoleForm = document.getElementById("consoleForm");
+	let consoleInput = document.getElementById("consoleInput");
 
-	var viewerImage = document.getElementById('viewerImage');
+	let viewerImage = document.getElementById("viewerImage");
 
 	viewerImage.width  = 1;
 	viewerImage.height = 1;
 
-	var context = viewerImage.getContext("2d");
-	var imageData = context.getImageData(0, 0, 1, 1); // only do this once per page
-	var target  = imageData.data; 
+	let context = viewerImage.getContext("2d");
+	let imageData = context.getImageData(0, 0, 1, 1); // only do this once per page
+	let target  = imageData.data; 
 
-	var iterationCounter = new IterationCounter();
-	var fpsCounter = new FpsCounter();
+	let iterationCounter = new IterationCounter();
+	let fpsCounter = new FpsCounter();
 
 	// Create a new WebSocket.
-	var socket = new WebSocket('ws://localhost:8080');
+	let socket = new WebSocket("ws://localhost:8080");
 	socket.binaryType = "arraybuffer";
 
 	// Handle any errors that occur.
 	socket.onerror = function(error) {
-		console.log('WebSocket Error: ' + error);
+		console.log("WebSocket Error: " + error);
 	};
 
 	// Show a connected message when the WebSocket is opened.
@@ -86,27 +86,30 @@ window.onload = function() {
 			iterationCounter.update();
 			fpsCounter.update();
 
-			var binaryData = new Uint8Array(event.data);
+			let binaryData = new Uint8Array(event.data);
 
 			target.set(binaryData);
 			context.putImageData(imageData, 0, 0);
 		} else {
-			var messageObject = JSON.parse(event.data);
+			let messageObject = JSON.parse(event.data);
 
-			var resolution = messageObject["resolution"];
+			let resolution = messageObject["resolution"];
+			if (undefined !== resolution) {
+				viewerImage.width  = resolution[0];
+				viewerImage.height = resolution[1];
 
-			viewerImage.width  = resolution[0];
-			viewerImage.height = resolution[1];
+				context.fillRect(0, 0, resolution[0], resolution[1]);
 
-			context.fillRect(0, 0, resolution[0], resolution[1]);
+				imageData = context.getImageData(0, 0, resolution[0], resolution[1]);
+				target = imageData.data;
 
-			imageData = context.getImageData(0, 0, resolution[0], resolution[1]);
-			target = imageData.data;
+				consoleInput.style["width"] = (resolution[0] - 4) + "px";
+			}
 
-			consoleInput.style['width'] = (resolution[0] - 4) + 'px';
-
-			var iteration = messageObject["iteration"];
-			iterationCounter.init(iteration);
+			let iteration = messageObject["iteration"];
+			if (undefined !== iteration) {
+				iterationCounter.init(iteration);
+			}
 		}
 	};
 
@@ -121,13 +124,13 @@ window.onload = function() {
 		e.preventDefault();
 
 		// Retrieve the message from the textarea.
-		var message = consoleInput.value;
+		let message = consoleInput.value;
 
     	// Send the message through the WebSocket.
 		socket.send(message);
 
 		// Clear out the message field.
-		consoleInput.value = '';
+		consoleInput.value = "";
 
 		return false;
 	};
