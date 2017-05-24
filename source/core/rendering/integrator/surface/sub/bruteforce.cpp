d6ed40a5460f842b03b6e8b51c9e2a9e7807e627
Opioid <benjamin.savs@gmail.com>
2017-05-24 23:12:59 +0200

    Maintenance

--- source/core/rendering/integrator/surface/sub/bruteforce.cpp
+++ source/core/rendering/integrator/surface/sub/bruteforce.cpp
@@ -36,7 +36,7 @@ float3 Bruteforce::li(Worker& worker, const Ray& ray, const Intersection& inters
 
 	const auto& bssrdf = intersection.bssrdf(worker);
 
-	float step_size = 1.f;
+	const float step_size = 0.1f;
 
 	const uint32_t num_samples = static_cast<uint32_t>(std::ceil(range / step_size));
 
