671522a370be93054498471d8ee3f5878ff47934
Opioid <benjamin.savs@gmail.com>
2017-10-06 14:24:27 +0200

    cmake maintenance

--- source/core/take/take.cmake
+++ source/core/take/take.cmake
@@ -1,8 +1,8 @@
 target_sources(core
-  PRIVATE
-  "${CMAKE_CURRENT_LIST_DIR}/take_loader.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/take_loader.hpp"
-  "${CMAKE_CURRENT_LIST_DIR}/take_settings.hpp"
-  "${CMAKE_CURRENT_LIST_DIR}/take_view.hpp"
-  "${CMAKE_CURRENT_LIST_DIR}/take.hpp"
-)    
+	PRIVATE
+	"${CMAKE_CURRENT_LIST_DIR}/take_loader.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/take_loader.hpp"
+	"${CMAKE_CURRENT_LIST_DIR}/take_settings.hpp"
+	"${CMAKE_CURRENT_LIST_DIR}/take_view.hpp"
+	"${CMAKE_CURRENT_LIST_DIR}/take.hpp"
+	)    
