29dfa97f25bd8c22be0e26fbcf742c3fd23fc8c5
Opioid <benjamin.savs@gmail.com>
2017-11-13 16:07:45 +0100

    Decided on folder where to place the dreamy aurora stuff

---
+++ source/extension/procedural/aurora/aurora.cmake
@@ -0,0 +1,5 @@
+target_sources(extension
+	PRIVATE
+	"${CMAKE_CURRENT_LIST_DIR}/volume_rasterizer.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/volume_rasterizer.hpp"
+	)
--- source/extension/procedural/sky/sky.cmake
+++ source/extension/procedural/sky/sky.cmake
@@ -1,17 +1,17 @@
 include("${CMAKE_CURRENT_LIST_DIR}/hosek/hosek.cmake")
 
 target_sources(extension
-  PRIVATE
-  "${CMAKE_CURRENT_LIST_DIR}/sky_material_base.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sky_material_base.hpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sky_material.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sky_material.hpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sky_model.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sky_model.hpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sky_provider.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sky_provider.hpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sky.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sky.hpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sun_material.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/sun_material.hpp"
-)
+	PRIVATE
+	"${CMAKE_CURRENT_LIST_DIR}/sky_material_base.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sky_material_base.hpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sky_material.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sky_material.hpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sky_model.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sky_model.hpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sky_provider.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sky_provider.hpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sky.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sky.hpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sun_material.cpp"
+	"${CMAKE_CURRENT_LIST_DIR}/sun_material.hpp"
+	)
