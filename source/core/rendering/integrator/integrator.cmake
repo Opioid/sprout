f6f622f8d0ebb4d2b221ad4d2fa0c5c657b193c6
Opioid <benjamin.savs@gmail.com>
2018-06-20 16:02:43 +0200

    Preparing for photon mapper instead

--- source/core/rendering/integrator/integrator.cmake
+++ source/core/rendering/integrator/integrator.cmake
@@ -1,9 +1,10 @@
+include("${CMAKE_CURRENT_LIST_DIR}/photon/photon.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/surface/surface.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/volume/volume.cmake")
 
 target_sources(core
-	PRIVATE
-	"${CMAKE_CURRENT_LIST_DIR}/integrator_helper.hpp"
-	"${CMAKE_CURRENT_LIST_DIR}/integrator.cpp"
-	"${CMAKE_CURRENT_LIST_DIR}/integrator.hpp"
-	) 
+    PRIVATE
+    "${CMAKE_CURRENT_LIST_DIR}/integrator_helper.hpp"
+    "${CMAKE_CURRENT_LIST_DIR}/integrator.cpp"
+    "${CMAKE_CURRENT_LIST_DIR}/integrator.hpp"
+    ) 
