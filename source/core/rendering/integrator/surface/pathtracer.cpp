ba5375480f320730de9caa200590a9c25f63ac8a
Opioid <benjamin.savs@gmail.com>
2018-09-01 17:46:17 +0200

    Caustics consistency between integrators

--- source/core/rendering/integrator/surface/pathtracer.cpp
+++ source/core/rendering/integrator/surface/pathtracer.cpp
@@ -73,7 +73,7 @@ float3 Pathtracer::integrate(Ray& ray, Intersection& intersection, Worker& worke
         float3 const wo = -ray.direction;
 
         bool const avoid_caustics = settings_.avoid_caustics && !primary_ray &&
-                                    worker.interface_stack().top_is_vacuum();
+                                    worker.interface_stack().top_is_vacuum_or_pure_specular();
 
         auto const& material_sample = intersection.sample(wo, ray, filter, avoid_caustics, sampler_,
                                                           worker);
