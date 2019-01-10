5eb22fbacedd2d619245b8ee800a52c08e49e64e
Opioid <benjamin.savs@gmail.com>
2019-01-10 16:15:38 +0100

    Moved some math print to cpp file

--- source/base/math/print.hpp
+++ source/base/math/print.hpp
@@ -2,58 +2,41 @@
 #define SU_BASE_MATH_PRINT_HPP
 
 #include <ostream>
+#include "matrix.hpp"
+#include "matrix3x3.hpp"
 #include "matrix4x4.hpp"
 #include "vector4.inl"
 
 template <typename T>
-std::ostream& operator<<(std::ostream& stream, const math::Vector2<T>& v) {
+std::ostream& operator<<(std::ostream& stream, const math::Vector2<T>& v) noexcept {
     return stream << "[" << v[0] << ", " << v[1] << "]";
 }
 
-inline std::ostream& operator<<(std::ostream& stream, const math::Vector3<uint8_t>& v) {
-    return stream << "[" << static_cast<uint32_t>(v[0]) << ", " << static_cast<uint32_t>(v[1])
-                  << ", " << static_cast<uint32_t>(v[2]) << "]";
-}
+std::ostream& operator<<(std::ostream& stream, const math::Vector3<uint8_t>& v) noexcept;
 
 template <typename T>
-std::ostream& operator<<(std::ostream& stream, const math::Vector3<T>& v) {
+std::ostream& operator<<(std::ostream& stream, const math::Vector3<T>& v) noexcept {
     return stream << "[" << v[0] << ", " << v[1] << ", " << v[2] << "]";
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& stream, const math::Vector4<T>& v) {
+std::ostream& operator<<(std::ostream& stream, const math::Vector4<T>& v) noexcept {
     return stream << "[" << v[0] << ", " << v[1] << ", " << v[2] << ", " << v[3] << "]";
 }
 
-inline std::ostream& operator<<(std::ostream& stream, const math::Vector3f_a& v) {
-    return stream << "[" << v[0] << ", " << v[1] << ", " << v[2] << "]";
-}
+std::ostream& operator<<(std::ostream& stream, const math::Vector3f_a& v) noexcept;
 
-inline std::ostream& operator<<(std::ostream& stream, const math::Vector4f_a& v) {
-    return stream << "[" << v[0] << ", " << v[1] << ", " << v[2] << ", " << v[3] << "]";
-}
+std::ostream& operator<<(std::ostream& stream, const math::Vector4f_a& v) noexcept;
 
-inline std::ostream& operator<<(std::ostream& stream, const math::Vector4i_a& v) {
-    return stream << "[" << v[0] << ", " << v[1] << ", " << v[2] << ", " << v[3] << "]";
-}
+std::ostream& operator<<(std::ostream& stream, const math::Vector4i_a& v) noexcept;
 
 template <typename T>
-std::ostream& operator<<(std::ostream& stream, const math::Matrix3x3<T>& m) {
+std::ostream& operator<<(std::ostream& stream, const math::Matrix3x3<T>& m) noexcept {
     return stream << "[" << m.m00 << ", " << m.m01 << ", " << m.m02 << ",\n"
                   << " " << m.m10 << ", " << m.m11 << ", " << m.m12 << ",\n"
                   << " " << m.m20 << ", " << m.m21 << ", " << m.m22 << "]";
 }
-/*
-template<typename T>
-std::ostream& operator<<(std::ostream& stream, const math::Matrix4x4<T>& m) {
-        return stream << "[" << m.m00 << ", " << m.m01 << ", " << m.m02 << ", " << m.m03 << ",\n"
-                                  << " " << m.m10 << ", " << m.m11 << ", " << m.m12 << ", " << m.m13
-<< ",\n"
-                                  << " " << m.m20 << ", " << m.m21 << ", " << m.m22 << ", " << m.m23
-<< ",\n"
-                                  << " " << m.m30 << ", " << m.m31 << ", " << m.m32 << ", " << m.m33
-<< "]";
-}
-*/
+
+std::ostream& operator<<(std::ostream& stream, float3x3 const& m) noexcept;
 
 #endif
