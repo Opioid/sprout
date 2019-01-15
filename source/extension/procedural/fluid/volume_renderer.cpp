bbb5807c45e2bd2545a4ed030d5b879bee023a97
Opioid <benjamin.savs@gmail.com>
2019-01-15 18:35:05 +0100

    Forgot

--- source/extension/procedural/fluid/volume_renderer.cpp
+++ source/extension/procedural/fluid/volume_renderer.cpp
@@ -22,12 +22,12 @@ void Volume_renderer::resolve(image::Byte4& target) const noexcept {
     for (int32_t i = 0; i < len; ++i) {
         float4 const value = float4(voxels_[i]);
 
-//        float4 const color = min(value, max_saturation_) / max_saturation_;
+        //        float4 const color = min(value, max_saturation_) / max_saturation_;
 
-//        float4 const srgb = spectrum::linear_to_gamma_sRGB(color);
+        //        float4 const srgb = spectrum::linear_to_gamma_sRGB(color);
 
-        float3 const rgb = value.xyz() / value[3];
-        float const alpha = std::min(value[3], max_saturation_) / max_saturation_;
+        float3 const rgb   = value.xyz() / value[3];
+        float const  alpha = std::min(value[3], max_saturation_) / max_saturation_;
 
         float4 const srgb = spectrum::linear_to_gamma_sRGB(float4(rgb, alpha));
 
@@ -41,7 +41,7 @@ void Volume_renderer::resolve(image::Float1& target) const noexcept {
     int32_t const len = dimensions_[0] * dimensions_[1] * dimensions_[2];
 
     for (int32_t i = 0; i < len; ++i) {
-        float const value = 0.f;//float(voxels_[i]);
+        float const value = 0.f;  // float(voxels_[i]);
 
         float const color = std::min(value, max_saturation_) / max_saturation_;
 
@@ -62,7 +62,7 @@ void Volume_renderer::splat(float3 const& uvw, Type const& value) noexcept {
 
     float constexpr weight = 1.f / (3.f * 3.f * 3.f);
 
-  //  float constexpr weight = 1.f;
+    //  float constexpr weight = 1.f;
 
     splat(c + int3(-1, -1, -1), weight * value);
     splat(c + int3(0, -1, -1), weight * value);
