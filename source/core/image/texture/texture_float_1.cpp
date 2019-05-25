8addca2e53ab454c3b08d50e9ed16b49f896cb6c
beni <benjamin.savs@gmail.com>
2019-05-25 23:06:10 +0200

    Does this fix the build

--- source/core/image/texture/texture_float_1.cpp
+++ source/core/image/texture/texture_float_1.cpp
@@ -10,38 +10,38 @@ Float1_t<T>::Float1_t(Image const& image) noexcept
 
 template <typename T>
 float Float1_t<T>::at_1(int32_t i) const noexcept {
-    return image_.load(i);
+	return Typed_texture<T>::image_.load(i);
 }
 
 template <typename T>
 float3 Float1_t<T>::at_3(int32_t i) const noexcept {
-    return float3(image_.load(i), 0.f, 0.f);
+	return float3(Typed_texture<T>::image_.load(i), 0.f, 0.f);
 }
 
 template <typename T>
 float Float1_t<T>::at_1(int32_t x, int32_t y) const noexcept {
-    return image_.load(x, y);
+	return Typed_texture<T>::image_.load(x, y);
 }
 
 template <typename T>
 float2 Float1_t<T>::at_2(int32_t x, int32_t y) const noexcept {
-    return float2(image_.load(x, y), 0.f);
+	return float2(Typed_texture<T>::image_.load(x, y), 0.f);
 }
 
 template <typename T>
 float3 Float1_t<T>::at_3(int32_t x, int32_t y) const noexcept {
-    return float3(image_.load(x, y), 0.f, 0.f);
+	return float3(Typed_texture<T>::image_.load(x, y), 0.f, 0.f);
 }
 
 template <typename T>
 void Float1_t<T>::gather_1(int4 const& xy_xy1, float c[4]) const noexcept {
-    image_.gather(xy_xy1, c);
+	Typed_texture<T>::image_.gather(xy_xy1, c);
 }
 
 template <typename T>
 void Float1_t<T>::gather_2(int4 const& xy_xy1, float2 c[4]) const noexcept {
     float v[4];
-    image_.gather(xy_xy1, v);
+	Typed_texture<T>::image_.gather(xy_xy1, v);
 
     c[0] = float2(v[0], 0.f);
     c[1] = float2(v[1], 0.f);
@@ -52,7 +52,7 @@ void Float1_t<T>::gather_2(int4 const& xy_xy1, float2 c[4]) const noexcept {
 template <typename T>
 void Float1_t<T>::gather_3(int4 const& xy_xy1, float3 c[4]) const noexcept {
     float v[4];
-    image_.gather(xy_xy1, v);
+	Typed_texture<T>::image_.gather(xy_xy1, v);
 
     c[0] = float3(v[0], 0.f, 0.f);
     c[1] = float3(v[1], 0.f, 0.f);
@@ -62,37 +62,37 @@ void Float1_t<T>::gather_3(int4 const& xy_xy1, float3 c[4]) const noexcept {
 
 template <typename T>
 float Float1_t<T>::at_element_1(int32_t x, int32_t y, int32_t element) const noexcept {
-    return image_.at_element(x, y, element);
+	return Typed_texture<T>::image_.at_element(x, y, element);
 }
 
 template <typename T>
 float2 Float1_t<T>::at_element_2(int32_t x, int32_t y, int32_t element) const noexcept {
-    return float2(image_.at_element(x, y, element), 0.f);
+	return float2(Typed_texture<T>::image_.at_element(x, y, element), 0.f);
 }
 
 template <typename T>
 float3 Float1_t<T>::at_element_3(int32_t x, int32_t y, int32_t element) const noexcept {
-    return float3(image_.at_element(x, y, element), 0.f, 0.f);
+	return float3(Typed_texture<T>::image_.at_element(x, y, element), 0.f, 0.f);
 }
 
 template <typename T>
 float Float1_t<T>::at_1(int32_t x, int32_t y, int32_t z) const noexcept {
-    return image_.load(x, y, z);
+	return Typed_texture<T>::image_.load(x, y, z);
 }
 
 template <typename T>
 float2 Float1_t<T>::at_2(int32_t x, int32_t y, int32_t z) const noexcept {
-    return float2(image_.load(x, y, z), 0.f);
+	return float2(Typed_texture<T>::image_.load(x, y, z), 0.f);
 }
 
 template <typename T>
 float3 Float1_t<T>::at_3(int32_t x, int32_t y, int32_t z) const noexcept {
-    return float3(image_.load(x, y, z), 0.f, 0.f);
+	return float3(Typed_texture<T>::image_.load(x, y, z), 0.f, 0.f);
 }
 
 template <typename T>
 float4 Float1_t<T>::at_4(int32_t x, int32_t y, int32_t z) const noexcept {
-    return float4(image_.load(x, y, z), 0.f, 0.f, 1.f);
+	return float4(Typed_texture<T>::image_.load(x, y, z), 0.f, 0.f, 1.f);
 }
 
 template class Float1_t<image::Float1>;
