f38a82910e9df765b7e3209b7647a61dcf146090
Opioid <benjamin.savs@gmail.com>
2020-02-19 13:53:44 +0100

    Simplified Flags implementation

--- source/base/flags/flags.hpp
+++ source/base/flags/flags.hpp
@@ -11,29 +11,26 @@ struct Flags {
 
     Flags() = default;
 
-    Flags(T flag) : values(static_cast<impl_type>(flag)) {}
+    Flags(T flag) : values(impl_type(flag)) {}
 
     constexpr bool empty() const {
         return impl_type(0) != values;
     }
 
     constexpr bool is(T flag) const {
-        return static_cast<impl_type>(flag) == (values & static_cast<impl_type>(flag));
+        return 0 != (values & impl_type(flag));
     }
 
     constexpr bool no(T flag) const {
-        return 0 == (values & static_cast<impl_type>(flag));
+        return 0 == (values & impl_type(flag));
     }
 
     constexpr bool any(T a, T b) const {
-        return (static_cast<impl_type>(a) == (values & static_cast<impl_type>(a))) |
-               (static_cast<impl_type>(b) == (values & static_cast<impl_type>(b)));
+        return 0 != (values & (impl_type(a) | impl_type(b)));
     }
 
     constexpr bool any(T a, T b, T c) const {
-        return (static_cast<impl_type>(a) == (values & static_cast<impl_type>(a))) |
-               (static_cast<impl_type>(b) == (values & static_cast<impl_type>(b))) |
-               (static_cast<impl_type>(c) == (values & static_cast<impl_type>(c)));
+        return 0 != (values & (impl_type(a) | impl_type(b) | impl_type(c)));
     }
 
     constexpr bool operator!=(Flags other) const {
@@ -41,19 +38,19 @@ struct Flags {
     }
 
     void set(T flag) {
-        values |= static_cast<impl_type>(flag);
+        values |= impl_type(flag);
     }
 
     void set(T flag, bool value) {
         if (value) {
-            values |= static_cast<impl_type>(flag);
+            values |= impl_type(flag);
         } else {
-            values &= ~static_cast<impl_type>(flag);
+            values &= ~impl_type(flag);
         }
     }
 
     void unset(T flag) {
-        values &= ~static_cast<impl_type>(flag);
+        values &= ~impl_type(flag);
     }
 
     void clear() {
@@ -61,7 +58,7 @@ struct Flags {
     }
 
     void clear(T flag) {
-        values = static_cast<impl_type>(flag);
+        values = impl_type(flag);
     }
 
     impl_type values = impl_type(0);
