e34f7ba149cdaa4daa61f0b2962d1efcb8860381
Opioid <benjamin.savs@gmail.com>
2021-04-25 15:15:05 +0200

    Straight_transmission had 1 plain bug and 1 problem with MIS

--- source/core/rendering/integrator/surface/pathtracer.cpp
+++ source/core/rendering/integrator/surface/pathtracer.cpp
@@ -151,18 +151,20 @@ float4 Pathtracer::integrate(Ray& ray, Intersection& isec, Worker& worker, AOV* 
             primary_ray = false;
         }
 
-        if (sample_result.type != Bxdf_type::Straight) {
+        if (sample_result.type != Bxdf_type::Straight_transmission) {
             ++ray.depth;
         }
 
-        if (sample_result.type.no(Bxdf_type::Straight)) {
+        if (sample_result.type.is(Bxdf_type::Straight)) {
+            ray.min_t() = offset_f(ray.max_t());
+        } else {
+            ray.origin  = isec.offset_p(sample_result.wi);
             ray.set_direction(sample_result.wi);
 
             transparent     = false;
             from_subsurface = false;
         }
 
-        ray.origin  = isec.offset_p(sample_result.wi);
         ray.max_t() = Ray_max_t;
 
         if (0.f == ray.wavelength) {
