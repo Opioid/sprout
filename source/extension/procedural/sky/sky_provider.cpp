d41ddc0b93e6245838e925a10f81dd005992ad1e
Opioid <benjamin.savs@gmail.com>
2021-04-28 23:21:23 +0200

    Formatting

--- source/extension/procedural/sky/sky_provider.cpp
+++ source/extension/procedural/sky/sky_provider.cpp
@@ -38,8 +38,7 @@ void Provider::set_material_provider(material::Provider& material_provider) {
     material_provider_ = &material_provider;
 }
 
-uint32_t Provider::create_extension(json::Value const& extension_value, Scene& scene,
-                                    Resources& resources) {
+uint32_t Provider::create_extension(json::Value const& value, Scene& scene, Resources& resources) {
     Sky* sky = new Sky;
 
     uint32_t const sky_entity = scene.create_extension(sky);
@@ -49,42 +48,40 @@ uint32_t Provider::create_extension(json::Value const& extension_value, Scene& s
     static char constexpr Sky_name[] = "proc:sky";
     static char constexpr Sun_name[] = "proc:sun";
 
-    uint32_t sky_material_id;
+    uint32_t sky_mat;
 
     if (auto const resource = resources.get<material::Material>(Sky_name); resource.ptr) {
-        sky_material_id = resource.id;
+        sky_mat = resource.id;
 
         static_cast<Material*>(resource.ptr)->set_sky(sky);
     } else {
         if (Bake) {
-            sky_material_id = resources.store<material::Material>(Sky_name,
-                                                                  new Sky_baked_material(sky));
+            sky_mat = resources.store<material::Material>(Sky_name, new Sky_baked_material(sky));
         } else {
-            sky_material_id = resources.store<material::Material>(Sky_name, new Sky_material(sky));
+            sky_mat = resources.store<material::Material>(Sky_name, new Sky_material(sky));
         }
     }
 
-    uint32_t sun_material_id;
+    uint32_t sun_mat;
 
     if (auto const resource = resources.get<material::Material>(Sun_name); resource.ptr) {
-        sun_material_id = resource.id;
+        sun_mat = resource.id;
 
         static_cast<Material*>(resource.ptr)->set_sky(sky);
     } else {
         if (Bake) {
-            sun_material_id = resources.store<material::Material>(Sun_name,
-                                                                  new Sun_baked_material(sky));
+            sun_mat = resources.store<material::Material>(Sun_name, new Sun_baked_material(sky));
         } else {
-            sun_material_id = resources.store<material::Material>(Sun_name, new Sun_material(sky));
+            sun_mat = resources.store<material::Material>(Sun_name, new Sun_material(sky));
         }
     }
 
-    uint32_t const sky_prop = scene.create_prop(scene_loader_->canopy(), &sky_material_id);
-    uint32_t const sun_prop = scene.create_prop(scene_loader_->distant_sphere(), &sun_material_id);
+    uint32_t const sky_prop = scene.create_prop(scene_loader_->canopy(), &sky_mat);
+    uint32_t const sun_prop = scene.create_prop(scene_loader_->distant_sphere(), &sun_mat);
 
     sky->init(sky_prop, sun_prop, scene);
 
-    if (auto const p = extension_value.FindMember("parameters"); extension_value.MemberEnd() != p) {
+    if (auto const p = value.FindMember("parameters"); value.MemberEnd() != p) {
         sky->set_parameters(p->value, scene);
     } else {
         sky->update(scene);
