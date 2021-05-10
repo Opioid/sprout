db1e54ae7b12c21a974a35695ad530dd7ff46086
Opioid <benjamin.savs@gmail.com>
2021-05-10 14:11:54 +0200

    When did that change, though?

--- source/core/testing/testing_size.cpp
+++ source/core/testing/testing_size.cpp
@@ -88,29 +88,29 @@ void size() {
 
     print_size<scene::prop::Intersection>("prop::Intersection", 112);
 
-    print_size<scene::material::Material>("material::Material", 112);
+    print_size<scene::material::Material>("material::Material", 128);
     print_size<scene::material::bxdf::Sample>("bxdf::Sample", 64);
 
-    print_size<scene::material::debug::Material>("debug::Material", 112);
+    print_size<scene::material::debug::Material>("debug::Material", 128);
     print_size<scene::material::debug::Sample>("debug::Sample", 160);
 
-    print_size<scene::material::glass::Glass>("glass::Glass", 144);
-    print_size<scene::material::glass::Sample>("glass::Sample", 160);
+    print_size<scene::material::glass::Glass>("glass::Glass", 160);
+    print_size<scene::material::glass::Sample>("glass::Sample", 176);
 
-    print_size<scene::material::light::Constant>("light::Constant", 144);
-    print_size<scene::material::light::Emissionmap>("light::Emissionmap", 208);
+    print_size<scene::material::light::Constant>("light::Constant", 160);
+    print_size<scene::material::light::Emissionmap>("light::Emissionmap", 224);
     print_size<scene::material::light::Sample>("light::Sample", 160);
 
-    print_size<scene::material::metal::Material_isotropic>("metal::Material_isotropic", 176);
-    print_size<scene::material::metal::Material_anisotropic>("metal::Material_anisotropic", 192);
+    print_size<scene::material::metal::Material_isotropic>("metal::Material_isotropic", 192);
+    print_size<scene::material::metal::Material_anisotropic>("metal::Material_anisotropic", 208);
     print_size<scene::material::metal::Sample_isotropic>("metal::Sample_isotropic", 192);
     print_size<scene::material::metal::Sample_anisotropic>("metal::Sample_anisotropic", 208);
 
-    print_size<scene::material::substitute::Material>("substitute::Material", 192);
+    print_size<scene::material::substitute::Material>("substitute::Material", 208);
     print_size<scene::material::substitute::Sample>("substitute::Sample", 208);
 
     print_size<scene::material::volumetric::Gridtree>("volumetric::Gridtree", 64);
-    print_size<scene::material::volumetric::Grid>("volumetric::Grid", 192);
+    print_size<scene::material::volumetric::Grid>("volumetric::Grid", 256);
 
     print_size<scene::prop::Prop>("prop::Prop", 8);
     print_size<scene::light::Light>("light::Light", 16);
